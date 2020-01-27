8014b19aec5b842cdc76c91c85364c539acd3bf0
wsatchmo <wsatcher@outlook.com>
2020-01-27 15:09:58 -0800

    Heroku

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "", //Change to your own
+    password: "MrMeseeksorgy", //Change to your own
     database: "burgers_db"
 });
 
